1dce2d226068ba7c37ef12f8fbd18dc502caffb1
darkmoon-6524 <gunnishmehta2003@gmail.com>
2024-02-10 08:13:50 +0530

    added python

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,4 +1,5 @@
 import {exec} from 'child_process'
+let stdout;
 
 export const signin = (req, res) => {
 
@@ -10,28 +11,34 @@ export const signup = (req, res) => {
     res.send('signup');
 }
 
-export const main = (req, res)=>{
+export const getList = (req, res)=>{
+  res.json(stdout);
+}
+
+
+export const main = (req, res) => {
     const pythonScriptPath = 'main.py';
 
     // Command to execute Python script
     const command = `python ${pythonScriptPath}`;
-  
+
     // Execute Python script
     exec(command, (error, stdout, stderr) => {
-      if (error) {
-        console.error(`Error executing Python script: ${error}`);
-        res.status(500).json({ error: 'Error executing Python script' });
-        return;
-      }
-  
-      // Output from Python script
-      console.log(`Output: ${stdout}`);
-      
-      if (stderr) {
-        console.error(`stderr: ${stderr}`);
-      }
-      
-      // Send output to frontend
-      res.json({ output: stdout });
+        if (error) {
+            console.error(`Error executing Python script: ${error}`);
+            res.status(500).json({ error: 'Error executing Python script' });
+            return;
+        }
+
+        // Output from Python script (matched names)
+        const matchedNames = stdout.trim().split('\n');
+        console.log(`Matched Names: ${matchedNames}`);
+
+        if (stderr) {
+            console.error(`stderr: ${stderr}`);
+        }
+
+        // Send matched names to frontend
+        res.json({ matchedNames });
     });
-}
+};
